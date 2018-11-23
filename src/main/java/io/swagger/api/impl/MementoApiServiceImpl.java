3d313e47239f395d7518af8c1cf9749d2fa5066d
ManuJazz <mjesusaz@alumnos.unex.es>
2018-11-23 16:07:33 +0000

    Commit 100

--- src/main/java/io/swagger/api/impl/MementoApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/MementoApiServiceImpl.java
@@ -3,12 +3,10 @@ package io.swagger.api.impl;
 import io.swagger.api.*;
 import io.swagger.model.*;
 
-import io.swagger.model.MementoItem;
-import io.swagger.model.ModelApiResponse;
-
-
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
+
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
@@ -17,47 +15,81 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-
 import javax.validation.constraints.*;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-11-23T14:41:06.005Z[GMT]")
 
 public class MementoApiServiceImpl extends MementoApiService {
-    
+    List<MementoItem> lm= new ArrayList<MementoItem>();
     @Override
     public Response addmemento(MementoItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        MementoItem mi= new MementoItem();
+        mi.setIdMemento(body.getIdMemento());
+        mi.setTitle(body.getTitle());
+        mi.setUrlRecordedVoice(body.getUrlRecordedVoice());
+        mi.setXCoordinate(body.getXCoordinate());
+        mi.setYCoordinate(body.getYCoordinate());
+        lm.add(mi);
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Memento Added!")).build();
     }
     
     @Override
     public Response deleteMemento(Long mementoId, String apiKey, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        
+        for (int i = 0; i < lm.size(); i++) {
+            if(lm.get(i).getIdMemento()==mementoId)
+                lm.remove(lm.get(i));
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "All Mementos Deleted!")).build();
     }
     
     @Override
     public Response getMementoById(Long mementoId, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        MementoItem mi= new MementoItem();
+        for (int i = 0; i < lm.size(); i++) {
+            if(lm.get(i).getIdMemento()==mementoId)
+            mi=lm.get(i);
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, mi.toString())).build();
     }
     
     @Override
     public Response getallmementos(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, lm.toString())).build();
     }
     
     @Override
     public Response updateMemento(MementoItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        MementoItem mi= new MementoItem();
+        for (int i = 0; i < lm.size(); i++) {
+            if(lm.get(i).getIdMemento()==body.getIdMemento()){
+                mi= lm.get(i);
+                mi.setIdMemento(body.getIdMemento());
+                mi.setTitle(body.getTitle());
+                mi.setUrlRecordedVoice(body.getUrlRecordedVoice());
+                mi.setXCoordinate(body.getXCoordinate());
+                mi.setYCoordinate(body.getYCoordinate());
+            }
+            
+        }        
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Memento Updated!")).build();
     }
     
     @Override
     public Response uploadRecordedVoice(Long mementoId, Object body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+MementoItem mi= new MementoItem();
+        for (int i = 0; i < lm.size(); i++) {
+            if(lm.get(i).getIdMemento()==mementoId){
+                mi= lm.get(i);
+                
+                mi.setUrlRecordedVoice(body.toString());
+            }
+            
+        }        
+        
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "RecordedVoice!")).build();
     }
     
 }
